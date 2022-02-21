6f714d53551c5320f2356d26d2a206dfb80950fd
Gerryron <Gerryron@btpnsyariah.com>
2022-02-22 06:08:29 +0700

    Module aliases with jsconfig

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import Layout from "../components/Layout";
-import styles from "../styles/404.module.css";
+import Layout from "@/components/Layout";
+import styles from "@/styles/404.module.css";
 
 export default function NotFoundPage() {
   return (
