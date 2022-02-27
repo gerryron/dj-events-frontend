d3c940a98778e3f040ceb11cf7b93258cd244f65
Gerryron <Gerryron@btpnsyariah.com>
2022-02-28 03:36:19 +0700

    Search Feature

--- components/Layout.js
+++ components/Layout.js
@@ -13,7 +13,12 @@ export default function Layout({
   return (
     <div>
       <Head>
-        <title>{title}</title>
+        <title>
+          DJ Events |
+          {' '}
+          {title}
+          `
+        </title>
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
       </Head>
@@ -29,7 +34,7 @@ export default function Layout({
 }
 
 Layout.defaultProps = {
-  title: 'DJ Events | Find the hottest parties',
+  title: 'Find the hottest parties',
   description: 'Find the latest DJ and other musical events',
   keywords: 'music, dj, edm, events',
 };
---
+++ pages/events/search.js
@@ -0,0 +1,52 @@
+import qs from 'qs';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import EventItem from '@/components/EventItem';
+import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
+
+export default function SearchPage({ events }) {
+  const router = useRouter();
+
+  return (
+    <Layout title="Search Results">
+      <Link href="/events">
+        <a>
+          {'<'}
+          {' '}
+          Go Back
+        </a>
+      </Link>
+      <h1>
+        Search Results for
+        {' '}
+        {router.query.term}
+      </h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+    </Layout>
+  );
+}
+
+export async function getServerSideProps({ query: { term } }) {
+  const query = qs.stringify({
+    filters: {
+      $or: [
+        { name: { $containsi: term } },
+        { performers: { $containsi: term } },
+        { description: { $containsi: term } },
+        { venue: { $containsi: term } },
+      ],
+    },
+  });
+
+  const res = await fetch(`${API_URL}/api/events?${query}&populate=*`);
+  const { data } = await res.json();
+
+  return {
+    props: { events: data },
+  };
+}
